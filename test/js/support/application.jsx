0e22203c2adb7d4277d2447614547802a0ee829e
abhchand <me@abhchand.me>
2021-01-14 22:47:52 -0500

    Updating component mount order to load `<ColorGrid />` last

--- test/js/support/application.jsx
+++ test/js/support/application.jsx
@@ -9,18 +9,28 @@ const renderApplication = () => {
   const container = document.createElement('div');
   document.body.appendChild(container);
 
-  // Create a containing component that renders all components
+  /*
+   * Create a containing component that renders all components.
+   *
+   * Order matters here - we render the `ColorGrid` last because
+   * it dispatches events to the other two components, which need
+   * to exist by the time it mounts.
+   *
+   * This won't match the production display. But since we don't care
+   * about the display order for testing, this is a cheap way of
+   * ensuring mount order.
+   */
   function App(_props) {
     return (
       <div id='color-component'>
         <div id='color-editor-component'>
           <ColorEditor />
         </div>
-        <div id='color-export-component'>
-          <ColorGrid />
-        </div>
         <div id='color-grid-component'>
           <ColorExport />
+        </div>
+        <div id='color-export-component'>
+          <ColorGrid />
         </div>
       </div>
     );
